680acfc26f06cbe7056af179d1b62eff5dc48a47
Özkan YILMAZ <115948168+ozkanyilmz@users.noreply.github.com>
2023-06-28 15:41:31 +0300

    Add files via upload

---
+++ js/site.js
@@ -0,0 +1,6 @@
+// alert("JavaScript Çalıştı");
+// tüm çalışmalarda önce java script in çalıştığını görmek için üstteki alert mesajını denemek yararlıdır.
+
+function sayHello() {
+    alert("Merhaba Dünya");
+}
